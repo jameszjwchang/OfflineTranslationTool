69e50e1aba010cefbdd46d8ffb65158b5d2e2feb
James Chang <jameszjwchang@outlook.com>
2024-12-08 15:03:40 +0800

    Initial Commit

---
+++ NLP2TeX/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NLP2TeX
+//
+//  Created by James Chang on 12/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NLP2TeX/NLP2TeXApp.swift
@@ -0,0 +1,17 @@
+//
+//  NLP2TeXApp.swift
+//  NLP2TeX
+//
+//  Created by James Chang on 12/8/24.
+//
+
+import SwiftUI
+
+@main
+struct NLP2TeXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
